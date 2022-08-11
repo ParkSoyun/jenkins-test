12a1eff57d16667d95e6339b2813c1f0305c080e
ParkSoyun <soyp@naver.com>
2022-08-11 21:35:12 +0900

    connect test2

--- src/main/java/com/example/jenkinstest/controller/TestController.java
+++ src/main/java/com/example/jenkinstest/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
     @GetMapping("/")
     public String hello(){
-        return "hello jenkins! nice to meet you!";
+        return "hello jenkins! nice to meet you!!";
     }
 }
