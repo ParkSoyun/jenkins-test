7eebf0ca0907dc42f6defdca18b3153201167392
ParkSoyun <soyp@naver.com>
2022-08-15 22:20:53 +0900

    create cpu bound application

--- src/main/java/com/example/jenkinstest/controller/TestController.java
+++ src/main/java/com/example/jenkinstest/controller/TestController.java
@@ -1,13 +1,13 @@
-package com.example.jenkinstest.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class TestController {
-
-    @GetMapping("/")
-    public String hello(){
-        return "hello jenkins! nice to meet you!!!!";
-    }
-}
+//package com.example.jenkinstest.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class TestController {
+//
+//    @GetMapping("/")
+//    public String hello(){
+//        return "hello jenkins! nice to meet you!!!!";
+//    }
+//}
